c7fa963a180574304c0f5cdc784307e593619eb1
rahulprakria <rahul.arya@prakria>
2024-09-14 16:27:44 +0530

    body parser

--- src/routes/stripe.route.ts
+++ src/routes/stripe.route.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import bodyParser from 'body-parser';
+
 import {
   CreateCheckOutSession,
   StripeWebhook,
@@ -6,7 +8,7 @@ import {
 import { VerifyUserHandler } from "../middlewares/verifyUser.middleware";
 
 const router = Router();
-router.post("/webhook", StripeWebhook);
+router.post("/webhook", bodyParser.raw({ type: 'application/json' }), StripeWebhook);
 router.post("/create-checkout-session", VerifyUserHandler, CreateCheckOutSession);
 
 export default router;

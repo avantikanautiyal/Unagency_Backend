2ae77ac3a099770af495af0ebffc48a505f31eab
rahulprakria <rahul.arya@prakria>
2024-09-14 15:59:31 +0530

    stripe routing updates

--- src/routes/stripe.route.ts
+++ src/routes/stripe.route.ts
@@ -4,42 +4,9 @@ import {
   StripeWebhook,
 } from "../controllers/stripe.controller";
 import { VerifyUserHandler } from "../middlewares/verifyUser.middleware";
-import { asyncHandler } from "../utils/asyncHandler";
-import Stripe from "stripe";
-import CheckoutSession from "../models/checkoutsession.model";
 
 const router = Router();
-router.post(
-  "/webhook",
-  VerifyUserHandler,
-  asyncHandler(async (req, res) => {
-    const stripe = new Stripe(`${process.env.stripe_secret_key}`);
-    const sig = req.headers["stripe-signature"] as string;
-    let event;
-    event = stripe.webhooks.constructEvent(
-      req.body,
-      sig,
-      process.env.stripe_webhook_endpoint_secret!
-    );
-
-    console.log(event.type);
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-      await CheckoutSession.create({
-        sessionId: session.id,
-        customerId: session.customer,
-        paymentStatus: session.payment_status,
-        amountTotal: session.amount_total,
-        currency: session.currency,
-      });
-      console.log("Payment session saved successfully");
-    }
-  })
-);
-router.post(
-  "/create-checkout-session",
-  VerifyUserHandler,
-  CreateCheckOutSession
-);
+router.post("/webhook", VerifyUserHandler, StripeWebhook);
+router.post("/create-checkout-session", VerifyUserHandler, CreateCheckOutSession);
 
 export default router;

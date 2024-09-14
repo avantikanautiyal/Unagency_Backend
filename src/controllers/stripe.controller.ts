88b2419c4e923627673986b7f70f2fa989ec0844
rahulprakria <rahul.arya@prakria>
2024-09-14 16:43:56 +0530

    payload to string

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -31,11 +31,13 @@ const CreateCheckOutSession = asyncHandler(async (req: RequestUser, res) => {
 });
 
 const StripeWebhook = asyncHandler(async (req, res) => {
+  const payload = req.body;
+  const payloadString = JSON.stringify(payload, null, 2);
   const stripe = new Stripe(`${process.env.stripe_secret_key}`);
   const sig = req.headers["stripe-signature"] as string;
   let event;
   event = await stripe.webhooks.constructEventAsync(
-    req.body,
+    payloadString,
     sig,
     process.env.stripe_webhook_endpoint_secret!
   );

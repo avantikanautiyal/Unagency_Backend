89cd44c2013e73f029d6dd9a9b49055cf1221392
rahulprakria <rahul.arya@prakria>
2024-09-16 22:45:58 +0530

    subscription model update

--- src/models/subscription.model.ts
+++ src/models/subscription.model.ts
@@ -18,7 +18,7 @@ const SubscriptionSchema = new Schema<ISubscription>({
   status: {
     type: String,
     required: true,
-    enum: ["active", "canceled", "pending"],
+    enum: ["active", "canceled", "pending", "incomplete", "incomplete", "incomplete_expired", "trialing", "past_due", "unpaid"],
   }, // Subscription status ('active', 'canceled', etc.)
   currentPeriodStart: { type: Date, required: true }, // Subscription start
   currentPeriodEnd: { type: Date, required: true }, // Subscription renewal/cancellation date

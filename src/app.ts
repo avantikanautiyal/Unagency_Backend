04672f334c9aaf729e87792b83ae6e9d1ef18aeb
rahulprakria <rahul.arya@prakria>
2024-10-04 11:48:23 +0530

    appts

--- src/app.ts
+++ src/app.ts
@@ -118,10 +118,11 @@ const StripeWebhook = asyncHandler(async (req, res) => {
             payment_method: paymentMethodId,
             customer: customerId,
             usage: "off_session", // Save the card for future use
-            automatic_payment_methods: {
-              enabled: true,
-              allow_redirects: "never", // Disable redirect-based payment methods like 3D Secure
-            },
+             return_url: 'http://localhost:5173/'
+            // automatic_payment_methods: {
+            //   enabled: true,
+            //   allow_redirects: "never", // Disable redirect-based payment methods like 3D Secure
+            // },
           });
 
           // Confirm the SetupIntent to ensure 3DS is handled

e05bca83c00e3eeefe48a563de71f9ad783d2c5b
sourav sharma <sourav.sharma@prakria.com>
2024-10-26 11:29:34 +0530

    added a task module

--- src/routes/tasks.route.ts
+++ src/routes/tasks.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { CreateTask, TaskList, TaskListByUserId } from "../controllers/tasks.controller";
+import { CreateTask, TaskList, TaskListByUserId, TaskListForResource, UpdateTask } from "../controllers/tasks.controller";
 import { VerifyRole } from "../middlewares/verifyUser.middleware";
 
 const router = Router();
 router.post("/", VerifyRole(["servicing"]), CreateTask);
 router.get("/", VerifyRole(["servicing", "resource"]), TaskList);
-router.get("/:userId", VerifyRole(["servicing", "resource"]), TaskListByUserId)
+router.get("/kanban", VerifyRole(["resource"]), TaskListForResource);
+router.get("/:userId", VerifyRole(["servicing", "resource"]), TaskListByUserId);
+router.put("/update/:taskId", VerifyRole(["servicing", "resource"]), UpdateTask);
 
 export default router;

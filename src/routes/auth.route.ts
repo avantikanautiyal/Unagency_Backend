bf50eb05acb3dc4ef74693d39de928378ffc9a8f
sourav sharma <souravsharma@souravs-MacBook-Air.local>
2024-11-10 22:47:25 +0530

    fixing a new registration

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { Register, Verify } from "../controllers/auth.controller";
+import { Register, Verify,NewRegister } from "../controllers/auth.controller";
 import { VerifyUserHandler } from "../middlewares/verifyUser.middleware";
+import { RegisterIfNot } from "../middlewares/register.middleware";
 const router = Router();
 
 //Desc: It allow system to check if user is aunthenticated or not;
-router.get("/verify", VerifyUserHandler, Verify);
+router.get("/verify",RegisterIfNot ,VerifyUserHandler, Verify);
 
 router.post("/register", Register);
+router.post("/register-login", NewRegister);
+
 export default router;

03bd475d63d072dee5a8b2c5741dddbe5f6bad78
sourav <souravsh1234567@gmail.com>
2025-11-03 18:09:32 +0530

    OAuth gmail email added for node mailer worker queuee

--- src/services/email/nodemailer.ts
+++ src/services/email/nodemailer.ts
@@ -1,38 +1,27 @@
 import nodemailer from "nodemailer";
-// const email = process.env.email;
-// const password = process.env.password;
+import "dotenv/config";
 
-// console.log(email, password)
-
-const email = "figma.dev2024@gmail.com";
-const password = "REDACTED";
-
-const EMAIL="souravsh1234567@gmail.com"
-const EMAIL_PASSWORD="ienl wmcc vara xxbk"
-const email_s = process.env.EMAIL ?? EMAIL;
-const pass = process.env.EMAIL_PASSWORD ?? EMAIL_PASSWORD;
-export const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  //   host: "smtp.gmail.com",
-  // port: 465,
-  // secure: true,
-  auth: {
-    user: email_s,
-    pass: pass,
-  },
-  // auth: {
-  //     user: email,
-  //     pass: password,
-  // },
-  //   service: "gmail",
-  //   auth: {
-  //     type: "OAuth2",
-  //     user: "your_email@gmail.com",
-  //     clientId:
-  //       "REDACTED",
-  //     clientSecret: "REDACTED",
-  //     refreshToken:
-  //       "REDACTED",
-  //     accessToken:"REDACTED"
-  //   },
+const  {
+  CLIENT_ID,
+  CLINET_SECRET,
+  REFRESH_TOKEN,
+  REDIRECT_URL,
+  TECH_SUPPORT_EMAIL
+} = process.env;
+console.log({
+  CLIENT_ID,
+  CLINET_SECRET,
+  REFRESH_TOKEN,
+  REDIRECT_URL,
+  TECH_SUPPORT_EMAIL
 });
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: TECH_SUPPORT_EMAIL,
+    clientId: CLIENT_ID,
+    clientSecret: CLINET_SECRET,
+    refreshToken: REFRESH_TOKEN,
+  },
+} as any);
